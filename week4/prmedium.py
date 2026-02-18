5a01e1dacb5a97d42febd762d95a11232da5cf84
seunggonhyeon <hyunsg5638@gmail.com>
2026-02-18 23:59:22 +0900

    feat:week4 prmedium.py (2)

--- week4/prmedium.py
+++ week4/prmedium.py
@@ -6,17 +6,22 @@ def solution(answers):
     
     methods = [[1,2,3,4,5], [2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
     
-    for i in range(len(answers)):
-        for sheet in methods:
-            if sheet[i] == answers[i]:
-                scores[i] += 1
+    for i, answer in enumerate(answers):
+            for idx, method in enumerate(methods):
+                # 나머지 연산자로 패턴 반복 처리
+                if method[i % len(method)] == answer:
+                    scores[idx] += 1
     
     # 0,5,1 개
     # 3등, 1등, 2등          
     
     # sorted_scores = sorted(scores,reverse=True)
-    sorted_scores = sorted(scores, reverse=True)
-    # [5,1,0]
+    max_score = max(scores)
+    # scores = [0,5,1] 점
+    # sorted_scores = [5,1,0] 점
+
+    # scores = [0,5,5] 점
+    # sorted_scores = [5,5,0] 점
 
     rank = []
 
@@ -26,9 +31,16 @@ def solution(answers):
     for i in range(len(scores)):
         rank[scores.index(sorted_scores[i])] = i+1
 
-    # [3,1,2]
+    # rank = [3,1,2]
+
+    winner = 0
+    answer = []
+
+    for i in rank:
+        if i == min(rank):
+            answer.append(i+1) 
     
-    answer = rank.index(min(rank))+1
+    # answer = rank.index(min(rank))+1
     
     
     
