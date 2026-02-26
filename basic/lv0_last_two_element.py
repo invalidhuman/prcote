6887a81d6fc19d14d6f4e23066534bf0683c24a7
seunggonhyeon <hyunsg5638@gmail.com>
2026-02-26 17:13:59 +0900

    week5 special.py, lv0 last two elements

---
+++ basic/lv0_last_two_element.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181927
+
+def solution(num_list):
+    
+    
+    
+    return num_list
+
+num_list = [2, 1, 6]
+print(num_list.append(2))
+
+num = 98
+n = 8
+print(num%n)
+print(not(num%n))
+print(int(not(num % n)))
