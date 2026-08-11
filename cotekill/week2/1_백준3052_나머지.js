29b36cb25cb82afd53602f4130f87df640322fae
seunggonhyeon <hyunsg5638@gmail.com>
2026-08-11 23:27:06 +0900

    260811: update 백준_나머지 다양한풀이

--- cotekill/week2/1_백준3052_나머지.js
+++ cotekill/week2/1_백준3052_나머지.js
@@ -7,8 +7,8 @@
  */
 
 // 방법1) 검사한 나머지들을 배열에 하나씩 추가하되, 매번 이미 있는 지 확인한다.
+// 방법 1-1) O(n^2) : includes() 사용
 function solution(numbers) {
-  // 1-1) O(n^2) : includes() 사용
   // includes() + for 문에 의해 1+2+3+...n-1번 검사하게 돼서 O(n^2)
   // n = 10으 고정이라 이 문제에서는 사용해도 괜찮음
   var remainders = [];
@@ -25,6 +25,31 @@ function solution(numbers) {
   }
 
   return remainders.length;
+}
+
+// 방법 1-2) O(n) : Set 사용
+function solution2(numbers) {
+  var remainders = new Set();
+  // 반복문 O(n)
+  for (const number of numbers) {
+    // 나머지 계산 : O(1)
+    // Set 추가 : 평균 O(1)
+    remainders.add(number % 42);
+  }
+
+  return remainders.size;
+}
+
+// 방법 2) 나머지가 0~41로 고정이니, 배열의 Index를 이용한다.
+function solution3(numbers) {
+  var remainders = Array(42).fill(false);
+
+  // 인덱스를 곧 나머지로 사용하니,
+  // 같은 나머지가 나올 경우 true로 지정된 요소에 또 true를 지정하게 될 뿐이다.
+  for (const number of numbers) {
+    remainders[number % 42] = true;
+  }
+  return remainders.filter(Boolean).length; // true만 filter한 뒤 길이 반환
 }
 
 const testCases = [
@@ -44,7 +69,8 @@ const testCases = [
 
 if (require.main === module) {
   testCases.forEach(({ args, expected }, index) => {
-    const actual = solution(...args);
+    // 아래 solution을 solution2, solution3 등으로 바꾸어 여러 테스트 케이스를 처리할 수 있습니다.
+    const actual = solution3(...args);
     const passed = JSON.stringify(actual) === JSON.stringify(expected);
 
     console.log(`예제 ${index + 1}: ${passed ? "PASS" : "FAIL"}`);
